32327bebf6f1f94722a53343a97b0f0b17bee98e
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-03-18 10:09:49 +0800

    HACK: gl/egl: allow direct dmabuf import when unable to query formats
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- gst-libs/gst/gl/egl/gsteglimage.c
+++ gst-libs/gst/gl/egl/gsteglimage.c
@@ -759,8 +759,8 @@ gst_egl_image_check_dmabuf_direct (GstGLContext * context,
   gst_eglQueryDmaBufModifiersEXT =
       gst_gl_context_get_proc_address (context, "eglQueryDmaBufModifiersEXT");
 
-  if (!gst_eglQueryDmaBufFormatsEXT || !gst_eglQueryDmaBufModifiersEXT)
-    return FALSE;
+  if (!gst_eglQueryDmaBufFormatsEXT)
+    return TRUE;
 
   display_egl = gst_gl_display_egl_from_gl_display (context->display);
   if (!display_egl) {
@@ -796,6 +796,9 @@ gst_egl_image_check_dmabuf_direct (GstGLContext * context,
         GST_FOURCC_ARGS (fourcc));
     return FALSE;
   }
+
+  if (!gst_eglQueryDmaBufModifiersEXT)
+    return TRUE;
 
   ret = gst_eglQueryDmaBufModifiersEXT (egl_display, fourcc, 0, NULL, NULL,
       &num_modifiers);
